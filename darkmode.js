f192ab549416fe20d1042ba868c4d4388ea8d17b
Aditi Mishra <68332164+aditimishra28@users.noreply.github.com>
2025-07-14 18:15:54 -0400

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,13 +1,13 @@
 function dark_mode() {
-	document.body.classList.toggle('dark');
-	var isDarkModeEnabled = document.body.classList.contains('dark');
-	sessionStorage.setItem('darkModeEnabled', isDarkModeEnabled);
+    document.body.classList.toggle('dark');
+    var isDarkModeEnabled = document.body.classList.contains('dark');
+    localStorage.setItem('darkModeEnabled', isDarkModeEnabled); // Use localStorage for persistence
 }
 
 function applyDarkModePreference() {
-	var isDarkModeStored = sessionStorage.getItem('darkModeEnabled');
-	if (isDarkModeStored && isDarkModeStored === 'true') {
-		document.body.classList.add('dark');
-	}
+    var isDarkModeStored = localStorage.getItem('darkModeEnabled'); // Check localStorage
+    if (isDarkModeStored && isDarkModeStored === 'true') {
+        document.body.classList.add('dark');
+    }
 }
 applyDarkModePreference();
